46ea3a2237b77ec1c1e77421afc6adb65e1f4849
kiranc1 <kiranc1@utas.edu.au>
2026-04-23 18:29:08 +1000

    tutorial 1 done

---
+++ tutorial 1/lab1.java
@@ -0,0 +1,13 @@
+/**
+ * KIT107 Lab 1
+ * @author Kiran Chhantyal
+ * @version tutorial 1
+ */
+
+public class Lab1
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World!");
+    }
+}
